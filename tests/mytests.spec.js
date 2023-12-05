3d6b5ede65b60bc586ac74c44f8ad0230b261753
Pavlobuch <146011284+Pavlobuch@users.noreply.github.com>
2023-12-05 21:50:01 +0200

    Unit 9 (#1)
    * Unit 9
    
    * second
    
    * new commit
    
    * new commit
    
    * baseURL added

---
+++ tests/mytests.spec.js
@@ -0,0 +1,47 @@
+const { test, expect} = require('@playwright/test');
+const exp = require('constants');
+
+test.beforeEach('Login on site', async ({ page }) => {
+    await page.goto('/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+
+    await page.locator('#login-button').click();
+  });
+
+test('Login with valid creds', async ({page}) => {
+
+    expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
+
+    const isVisibleTitle = await page.isVisible('.title');
+    expect(isVisibleTitle).toBe(true);
+
+    const isVisibleIcon = await page.isVisible('.shopping_cart_container');
+    expect(isVisibleTitle).toBe(true);
+
+    const numberOfProducts = (await page.$$('.inventory_item')).length
+    expect(numberOfProducts).toBeGreaterThan(1)
+});
+
+test('Add product to the cart', async ({page}) => {
+
+    expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
+
+    await page.locator('#add-to-cart-sauce-labs-backpack').click();
+    const addedItem = page.locator('#item_4_title_link')
+
+    const cartItems = page.locator('.shopping_cart_badge');
+    expect(cartItems).toHaveText('1');
+
+    await page.locator('.shopping_cart_link').click();
+    expect(page.url()).toBe('https://www.saucedemo.com/cart.html');
+
+    expect(page.locator('#item_4_title_link')).toStrictEqual(addedItem);
+
+    await page.locator('#remove-sauce-labs-backpack').click();
+    expect(await page.locator('.shopping_cart_badge').count()).toBe(0);
+    
+
+});
